911f4c5e4d6138c69e8b41a003fdde55fc71a13f
sarakhanX <sarawut.khan@hotmail.com>
2024-04-19 14:26:43 +0700

    fix cors

--- routers/pdf-test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const pdfController_1 = require("../controllers/pdfController");
-const router = (0, express_1.Router)();
-router.get('/genarate-pdf/:id', pdfController_1.docsControllerTest);
-exports.default = router;
--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ app.use(express.json());
 app.disable('etag');
 
 const corsOption = {
-  origin: true,
+  origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 200,
